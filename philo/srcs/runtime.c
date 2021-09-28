303a983b3e4ae09416fb8f00cab7d448b2c92f50
dalexhd <eldalexhd@gmail.com>
2021-09-28 21:02:08 +0200

    feat: fix double die.

--- philo/srcs/runtime.c
+++ philo/srcs/runtime.c
@@ -6,7 +6,7 @@
 /*   By: aborboll <aborboll@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 15:34:14 by aborboll          #+#    #+#             */
-/*   Updated: 2021/09/28 18:09:19 by aborboll         ###   ########.fr       */
+/*   Updated: 2021/09/28 20:59:39 by aborboll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,6 +67,8 @@ t_bool	thinking(t_philo *philo)
 
 t_bool	died(t_philo *philo)
 {
+	if (philo->any_died[0])
+		return (false);
 	philo->status = "died";
 	report_status(philo);
 	philo->any_died[0] = true;

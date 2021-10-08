3a94b15a94e9552c8ccd40ce7adc04cf6652b523
dalexhd <eldalexhd@gmail.com>
2021-10-08 09:36:03 +0200

    feat: fix norma.

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: aborboll <aborboll@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/12 11:45:43 by aborboll          #+#    #+#             */
-/*   Updated: 2021/10/08 07:30:16 by aborboll         ###   ########.fr       */
+/*   Updated: 2021/10/08 09:31:51 by aborboll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int	main(int argc, char **argv)
 	t_core	*core;
 	int		status;
 
+	status = 0;
 	if (validate_args(argc, argv))
 	{
 		core = initialize(argc, argv);
